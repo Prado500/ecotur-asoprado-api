4e5c0089c41a143d32665c1ddf323536687cd0d2
Prado500 <daldeoscoder@gmail.com>
2026-04-13 09:04:57 -0500

    chore(schemas): revisar importaciones de librerías y eliminar aquellas innecesarias

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 from datetime import datetime
 from enum import Enum
-
 class UserRole(str, Enum):
     admin = "admin"
     tourist = "tourist"
